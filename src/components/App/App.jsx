2955f3a6eb188c506485cf4b20a554e59521753b
YevheniiaYakymenko <152288008+YevheniiaYakymenko@users.noreply.github.com>
2024-06-22 22:36:07 +0200

    task

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,58 @@
+import { useState, useEffect } from "react";
 import Description from "../Descripion/Descripion";
+import Options from "../Options/Options";
+import Feedback from "../Feedback/Feedback";
+import Notification from "../../Notification/Notification";
 
 export default function App() {
-  return <Description />;
+  const initialFeedback = JSON.parse(localStorage.getItem("feedback")) || {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  const [feedback, setFeedback] = useState(initialFeedback);
+
+  useEffect(() => {
+    localStorage.setItem("feedback", JSON.stringify(feedback));
+  }, [feedback]);
+
+  const updateFeedback = (feedbackType) => {
+    setFeedback((firstFeedback) => ({
+      ...firstFeedback,
+      [feedbackType]: firstFeedback[feedbackType] + 1,
+    }));
+  };
+
+  const resetFeedback = () => {
+    setFeedback({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  };
+
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+  const positiveFeedback =
+    totalFeedback > 0 ? Math.round((feedback.good / totalFeedback) * 100) : 0;
+
+  return (
+    <>
+      <Description />
+      <Options
+        updateFeedback={updateFeedback}
+        resetFeedback={resetFeedback}
+        totalFeedback={totalFeedback}
+      />
+      {totalFeedback > 0 ? (
+        <Feedback
+          feedback={feedback}
+          totalFeedback={totalFeedback}
+          positiveFeedback={positiveFeedback}
+        />
+      ) : (
+        <Notification message="No feedback yet" />
+      )}
+    </>
+  );
 }
